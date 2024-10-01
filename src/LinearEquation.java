0d3c045143687f3837eeb404a06541ebec94ccd4
BT_2S14_21 <michaelk368@nycstudents.net>
2024-10-01 10:55:09 -0400

    Added almost entire functionality of LinearEquation class.

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -10,10 +10,52 @@ public class LinearEquation {
         this.y1 = y1;
         this.y2 = y2;
     }
- public double calculateSlope(){
+    public double calculateSlope()
+    {
         double slope = (double) (y2-y1/x2-x1);
         return slope;
- }
+    }
+    public double calculateYInt()
+    {
+        double YInt = y1 - (calculateSlope()*x1);
+        return YInt;
+    }
+    public void equation()
+    {
+        if (calculateYInt()>0){
+            System.out.println("y = " + calculateSlope() + " x" + calculateYInt());
+        }
+        else if (calculateYInt()<0) {
+            System.out.println("y = " + calculateSlope() + " x" + " - " + Math.abs(calculateYInt()));
+        }
+        else {
+            System.out.println("y = " + calculateSlope() + " x");
+        }
+    }
+    public double calculateDistance()
+    {
+        double distance = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+        return distance;
+    }
+    public double calculateThirdPoint(double x3)
+    {
+        double y3 = calculateSlope() * x3 + calculateYInt();
+        return y3;
+    }
+    public void printCoordinates(double x, double y)
+    {
+        System.out.print("(" + x + ", " + y + ")");
+    }
+    public String toString()
+
+    {
+        System.out.println("Your first coordinates were, ");
+        printCoordinates(x1,y1);
+        System.out.println("Your second coordinates were, ");
+        printCoordinates(x2,y2);
+
+
+    }
 }
 
 
