97172469e8b9025f6e68bc7755d52af3b5287d0c
BT_2S14_21 <michaelk368@nycstudents.net>
2024-10-01 10:35:43 -0400

    Initial commit

---
+++ src/EquationRunner.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class EquationRunner {
+    public static void main(String[] args) {
+
+LinearEquation b = new LinearEquation(1,2,1,2);
+        System.out.println(b.calculateSlope());
+    }
+}
---
+++ src/LinearEquation.java
@@ -0,0 +1,19 @@
+public class LinearEquation {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public LinearEquation(int x1, int x2, int y1, int y2){
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+ public double calculateSlope(){
+        double slope = (double) (y2-y1/x2-x1);
+        return slope;
+ }
+}
+
+
