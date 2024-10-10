4d638ce28ad083cb4aa585f8e15e9a93463ab12c
MichaelKarabinosh <michaelk368@nycstudents.net>
2024-10-10 18:59:51 -0400

    Added comments for ease of reading, fixed formating up, and fixed code logic so it follows all challenge criteria and to remove redundancies.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,45 +1,41 @@
-
 import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("----------------------------------------------------");
+
+        Scanner s = new Scanner(System.in); // Create a scanner object for user input
+        System.out.println("----------------------------------------------------"); // Introduction message
         System.out.println("Hello and welcome to the Linear Equation Calculator!");
         System.out.print("To begin, enter your first coordinate point: ");
 
-        String firstCoordinates = s.nextLine();
+
+        String firstCoordinates = s.nextLine(); // Get first coordinate from the user
         System.out.print("Enter your second coordinate point: ");
 
-        String secondCoordinates = s.nextLine();
+
+        String secondCoordinates = s.nextLine(); // Get second coordinate from the user
         System.out.println("----------------------------------------------------");
 
-        int comma1 = firstCoordinates.indexOf(",");
-        int x1 = Integer.parseInt(firstCoordinates.substring(1,comma1));
-        int y1 = Integer.parseInt(firstCoordinates.substring((comma1 + 1), (firstCoordinates.length()-1)));
+
+        int comma1 = firstCoordinates.indexOf(","); // Locating the comma to spilt x and y
+        int x1 = Integer.parseInt(firstCoordinates.substring(1,comma1)); // The ( is in index 0 always, so we start from idx 1 until the comma
+        int y1 = Integer.parseInt(firstCoordinates.substring((comma1 + 1), (firstCoordinates.length()-1))); // Here we parse from right after the comma to before the parentheses, which is always at idx length, so we do length - 1
 
 
-        int comma2 = secondCoordinates.indexOf(",");
+        int comma2 = secondCoordinates.indexOf(","); // Same approach as above
         int x2 = Integer.parseInt(secondCoordinates.substring(1,comma2));
         int y2 = Integer.parseInt(secondCoordinates.substring((comma2 + 1), (secondCoordinates.length()-1)));
 
 
-        LinearEquation z = new LinearEquation(x1,y1,x2,y2);
+        LinearEquation z = new LinearEquation(x1,y1,x2,y2); // Create an instance of LinearEquation class using the two points
         System.out.println(z.toString());
 
-        System.out.print("Enter a third x-value: ");
+
+        System.out.print("Enter a third x-value: "); // Ask the user for a third x-value to calculate the y-value
         String thirdCoordinate = s.nextLine();
-        double x3 = Double.parseDouble(thirdCoordinate);
+        double x3 = Double.parseDouble(thirdCoordinate); // Easier to parse because we're not dealing with multiple points or parentheses
         System.out.println("Solved coordinate point is: " + z.thirdCoordinate(x3));
         System.out.println("----------------------------------------------------");
-        s.close();
-
-
-
-
-
-
-
-
+        s.close(); // Closing scanner to prevent memory leak
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,104 +1,81 @@
 public class LinearEquation {
-    private int x1;
+    private int x1; // Instance variables for coordinates of two points
     private int x2;
     private int y1;
     private int y2;
 
-    public LinearEquation(int x1, int y1, int x2, int y2){
+    public LinearEquation(int x1, int y1, int x2, int y2) // Constructor creates the format for a LinearEquation object, using the 4 instance variables
+    {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
     }
-    public double calculateSlope()
+
+    public double calculateSlope() // Calculates the slope of the line using rise/run
     {
-        double slope = (double)(y2-y1)/(double)(x2-x1);
-        return slope;
-    }
-    public double calculateYInt()
-    {
-        double YInt = y1 - (calculateSlope()*x1);
-        return YInt;
-    }
-    public String equation()
-    {
-        if (calculateYInt()>0)
-        {
-            String equation = "y = " + fractionSlope() + "x + " + calculateYInt();
-            return equation;
-        }
-        else if (calculateYInt()<0) {
-            String equation = "y = " + fractionSlope() + "x" + " - " + Math.abs(calculateYInt());
-            return equation;
-        }
-        else {
-            String equation = "y = " + fractionSlope() + "x";
-            return equation;
-        }
-    }
-    public double calculateDistance()
-    {
-        double distance = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
-        return distance;
-    }
-    public double calculateThirdPoint(double x3)
-    {
-        double y3 = calculateSlope() * x3 + calculateYInt();
-        return y3;
-    }
-    public String printCoordinates(double x, double y)
-    {
-        String coords = "(" + x + ", " + y + ")";
-        return coords;
+        return (double)(y2-y1)/(double)(x2-x1);
     }
 
-    public String fractionSlope()
+    public double calculateYInt() // Calculates the y-intercept of the line
+    {
+        return y1 - calculateSlope()*x1;
+    }
+
+    public String equation() // Generates the equation of the line in slope-intercept form according to challenge functionality
+    {
+        if (calculateYInt()>0) // If the y-int is > 0, like 1.5, the equation will be printed with y = mx + b
+        {
+            return "y = " + fractionSlope() + "x + " + String.format("%.2f",calculateYInt());
+        }
+        else if (calculateYInt()<0) { // If the y-int is negative, we return the equation as y = mx - |b|, in order to avoid the y = mx + -b, following challenge functionality
+            return "y = " + fractionSlope() + "x" + " - " + String.format("%.2f",Math.abs(calculateYInt()));
+        }
+        else { // Lastly y-intercept = 0, we print y = mx, instead of y = mx + 0, according to challenge functionality
+            return "y = " + fractionSlope() + "x";
+        }
+    }
+    public double calculateDistance() // Calculates distance between the two points using distance formula
+    {
+        return Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+    }
+
+    public String fractionSlope() // Calculates the slope to be used in printing the equation. Outputs a numerically identical slope as above method, just with proper fraction and negative sign formatting, following challenge functionality
     {
         int num = y2-y1;
         int denom = x2-x1;
-        if (num % denom == 0)
+        if (num % denom == 0) // Basically checks if the slope is an int
         {
-            if (num/denom == 1)
+            if (num/denom == 1) // Returns no slope, so x instead of 1x if slope = 1
             {
                 return "";
             }
-            else {
+            else { // Returns slope as mx otherwise
                 return String.valueOf(num/denom);
             }
-
         }
-        if ((double)(num)/denom > 0)
+        if (denom < 0) // Switches the signs of the numerator and denominator so if the denom is < 0, the num inherits the negative sign
         {
-            return -1 * num + "/" + -1 * denom;
+            return -num + "/" + -denom;
         }
-        else if (denom < 0){
-            return -1 * num + "/" + -1 * denom;
-        }
-        else {
+        else { // Prints the slope as a fraction after all other cases have been checked
             return num + "/" + denom;
         }
     }
-public String test() {
-        return fractionSlope();
-    }
-    public String thirdCoordinate(double x3)
+
+    public String thirdCoordinate(double x3) // Calculates coordinate for third x value
     {
         double y3 = calculateSlope() * x3 + calculateYInt();
-    return "(" + String.format("%.2f",x3)  + ", " + String.format("%.2f",y3) + ")";
+        return "(" + String.format("%.2f",x3)  + ", " + String.format("%.2f",y3) + ")";
     }
-    public String toString()
 
+    public String toString() // Returns the original coordinates, y intercept, slope of the line, distance between the two points, and the equation of the line, using neat formatting,
     {
         String a = "Your first coordinates were (" + x1 + "," + y1 + ")" + "\n" + "Your second coordinates were (" + x2 + "," + y2 + ")";
         String b = "\nThe slope of the line is: " + String.format("%.2f", calculateSlope()) + "\n" + "The y-intercept is: " + String.format("%.2f", calculateYInt());
         String c = "\nThe distance between the two points is: " + String.format("%.2f", calculateDistance());
         String d = "\nThe equation of the line is: " + equation();
         String e = "\n----------------------------------------------------";
-        String allInfo = a + b + c + d + e;
-        return allInfo;
-
-
+        return a + b + c + d + e;
     }
 }
-
-
